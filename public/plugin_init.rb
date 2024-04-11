1fab1471e1dd778653e02497841e63336a777eef
Joshua Shaw <joshua.d.shaw@dartmouth.edu>
2024-04-11 16:27:05 -0400

    first pass at changing to index based search

--- public/plugin_init.rb
+++ public/plugin_init.rb
@@ -1,36 +1,4 @@
-# list out the types of things we want to negate
-# with statements like NOT "{type}/{date_like_number}"
-AppConfig[:aspace_search_modifications_primary_types] = [
-  'accessions',
-  'agents/families',
-  'agents/corporate_entitites',
-  'agents/people',
-  'archival_objects',
-  'classifications',
-  'digital_objects',
-  'digital_object_components',
-  'locations',
-  'objects',
-  'resources',
-  'record_groups',
-  'subjects',
-  'top_containers'
-]
-
 Rails.application.config.after_initialize do
-
-  class SearchController
-
-    alias_method :pre_aspace_search_mod_search, :search
-
-    def search
-      if params[:reset] == 'true' && params[:q] && params[:q].length > 0
-        params[:q] = remove_added_custom_params(params)[:q]
-      end
-      pre_aspace_search_mod_search
-    end
-
-  end
 
   Searchable.module_eval do
     alias_method :pre_aspace_search_mod_set_up_advanced_search, :set_up_advanced_search
@@ -40,7 +8,7 @@ Rails.application.config.after_initialize do
       pre_aspace_search_mod_set_up_advanced_search(default_types, default_facets, default_search_opts, params)
     end
 
-    # patched
+    # override
     def set_search_statement
       rid = defined?(@repo_id) ? @repo_id : nil
   #    Pry::ColorPrinter.pp @search
@@ -50,7 +18,7 @@ Rails.application.config.after_initialize do
   
       Rails.logger.debug("TYPE: #{type}")
       condition = " "
-      @search[:q].each_with_index do |q, i|
+      @original_params[:q].each_with_index do |q, i|
         # patch by skipping all added search statements
         next if i >= @search_length
         # end patch
@@ -75,40 +43,28 @@ Rails.application.config.after_initialize do
       @search[:search_statement] = I18n.t('search_results.search_for', :type => type,
                                           :conditions => "<ul class='no-bullets'>#{condition}</ul>")
     end
-
-    def remove_added_custom_params(params)
-      indices_to_remove = []
-
-      AppConfig[:aspace_search_modifications_primary_types].each do |type|
-        params[:q].each_with_index do |query, idx|
-          if query.include?('NOT "' + type + '/')
-            indices_to_remove << idx
-          end
-        end
-      end
-  
-      params[:q] = params[:q].delete_if.with_index{|_, i| indices_to_remove.include?(i) }
-      params[:op] = params[:op].delete_if.with_index{|_, i| indices_to_remove.include?(i) }
-  
-      params
-    end
   
     def add_date_like_omissions(params)
-      primary_types = AppConfig[:aspace_search_modifications_primary_types]
 
-      params = remove_added_custom_params(params)
+      @original_params = params
 
-      @search_length = params[:q].length
+      potential_dates = {}
+      potential_dates[:q] = []
+      potential_dates[:op] = []
+      potential_dates[:field] = []
 
-      # basic regex for dates that can be by themselves or have a quote on one side or the other
-      potential_dates = params[:q].map{ |d| " " + d + " "}.join.scan(/\s+"*(\d{4})"*\s+/).flatten
-
-      # add the negations to the query
-      primary_types.each_with_index do |type|
-        potential_dates.each_with_index do |date|
-          params[:q] << 'NOT "' + type + '/' + date.squish + '"'
-          params[:op] << "AND"
+      params[:q].each_with_index do |query, idx|
+        query.scan(/\s+(\d{4})\s+/).each do |date|
+          params[:q][idx].gsub!(date, '')
+          potential_dates[:q] << date
+          potential_dates[:op] << params[:op][idx]
+          potential_dates[:field] << 'extracted_dates_ustr'
         end
+      end
+      unless potential_dates.length = 0
+        params[:q] = params[:q] + potential_dates[:q]
+        params[:op] = params[:op] + potential_dates[:op]
+        params[:field] = params[:field] + potential_dates[:field]
       end
 
       params
